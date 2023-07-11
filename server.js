06cac6b79342a86e2fd52ab808061ae4fb828da6
Jhon Edward Mora Mora <jhonedmora@gmail.com>
2023-07-11 14:28:15 -0500

    Added DB Functionality (#2)
    * Updated auth functions to comply with newer versions of Auth0
    
    * Changed response methods to avoid using deprecated functions
    
    * Fixed logic regarding authentication
    
    * Added database funcitonality

--- server.js
+++ server.js
@@ -3,6 +3,19 @@
 var express = require('express');
 var app = express();
 var { auth } = require('express-oauth2-jwt-bearer');
+//Create connection to DB
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+connection.connect((err) => {
+    if (err) {
+        console.log("An error has ocurred connecting to DB, please check credentials");
+        throw err;
+    }
+})
 
 // We’ll create a middleware function to validate the access token when our API is called
 // Note that the audience field is the identifier you gave to your API.
@@ -22,7 +35,7 @@ var guard = function (req, res, next) {
     //Check all paths of this app
     if (req.path == '/movies' || req.path == '/reviewers' || req.path == '/publications') {
         //check if either of the permissions exist in the scope of the auth received
-        if (req.auth.payload.scope.includes(genPerms)||req.auth.payload.scope.includes(adminPerms)) {
+        if (req.auth.payload.scope.includes(genPerms) || req.auth.payload.scope.includes(adminPerms)) {
             next();
         } else {
             res.status(403).send({ message: 'Forbidden' });
@@ -46,71 +59,89 @@ app.use(function (err, req, res, next) {
     }
 });
 
+function getMoviesFromDB(callback) {
+    connection.query("select * from " + process.env.DB_NAME + ".moviereview where pending = false", function (err, rows) {
+        callback(err, rows);
+    });
+}
 
 
 // Implement the movies API endpoint
 app.get('/movies', function (req, res) {
-    // Get a list of movies and their review scores
-    //console.log("Returned movie list in JSON format!")
-    var movies = [
-        { title: 'Suicide Squad', release: '2016', score: 8, reviewer: 'Robert Smith', publication: 'The Daily Reviewer' },
-        { title: 'Batman vs. Superman', release: '2016', score: 6, reviewer: 'Chris Harris', publication: 'International Movie Critic' },
-        { title: 'Captain America: Civil War', release: '2016', score: 9, reviewer: 'Janet Garcia', publication: 'MoviesNow' },
-        { title: 'Deadpool', release: '2016', score: 9, reviewer: 'Andrew West', publication: 'MyNextReview' },
-        { title: 'Avengers: Age of Ultron', release: '2015', score: 7, reviewer: 'Mindy Lee', publication: 'Movies n\' Games' },
-        { title: 'Ant-Man', release: '2015', score: 8, reviewer: 'Martin Thomas', publication: 'TheOne' },
-        { title: 'Guardians of the Galaxy', release: '2014', score: 10, reviewer: 'Anthony Miller', publication: 'ComicBookHero.com' },
-    ]
-
-    // Send the response as a JSON array
-    res.json(movies);
+    getMoviesFromDB(function (err, result) {
+        if (err) {
+            res.status(500).json({ message: err })
+        } else {
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(409).json({ message: "Movies not found" })
+            }
+        }
+    })
 })
 
 // Implement the reviewers API endpoint
-app.get('/reviewers', function (req, res) {
-    // Get a list of all of our reviewers
-    var authors = [
-        { name: 'Robert Smith', publication: 'The Daily Reviewer', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/angelcolberg/128.jpg' },
-        { name: 'Chris Harris', publication: 'International Movie Critic', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/bungiwan/128.jpg' },
-        { name: 'Janet Garcia', publication: 'MoviesNow', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/grrr_nl/128.jpg' },
-        { name: 'Andrew West', publication: 'MyNextReview', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/d00maz/128.jpg' },
-        { name: 'Mindy Lee', publication: 'Movies n\' Games', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/laurengray/128.jpg' },
-        { name: 'Martin Thomas', publication: 'TheOne', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/karsh/128.jpg' },
-        { name: 'Anthony Miller', publication: 'ComicBookHero.com', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/9lessons/128.jpg' }
-    ];
+function getReviewersFromDB(callback) {
+    connection.query("select * from " + process.env.DB_NAME + ".reviewer", function (err, rows) {
+        callback(err, rows);
+    });
+}
 
-    // Send the list of reviewers as a JSON array
-    res.json(authors);
+app.get('/reviewers', function (req, res) {
+    getReviewersFromDB(function (err, authors) {
+        if (err) {
+            res.status(500).json({ message: err });
+        } else {
+            if (authors) {
+                res.status(200).json(authors);
+            } else {
+                res.status(409).json({ message: "Authors not found" });
+            }
+        }
+    });
 })
 
 // Implement the publications API endpoint
-app.get('/publications', function (req, res) {
-    // Get a list of publications
-    var publications = [
-        { name: 'The Daily Reviewer', avatar: 'glyphicon-eye-open' },
-        { name: 'International Movie Critic', avatar: 'glyphicon-fire' },
-        { name: 'MoviesNow', avatar: 'glyphicon-time' },
-        { name: 'MyNextReview', avatar: 'glyphicon-record' },
-        { name: 'Movies n\' Games', avatar: 'glyphicon-heart-empty' },
-        { name: 'TheOne', avatar: 'glyphicon-globe' },
-        { name: 'ComicBookHero.com', avatar: 'glyphicon-flash' }
-    ];
+function getPublicationsFromDB(callback) {
+    connection.query("SELECT * FROM movie_db.publication", function (err, rows) {
+        callback(err, rows);
+    });
+}
 
-    // Send the list of publications as a JSON array
-    res.json(publications);
+app.get('/publications', function (req, res) {
+    getPublicationsFromDB(function (err, publications) {
+        if (err) {
+            res.status(500).json({ message: err });
+        } else {
+            if (publications) {
+                res.status(200).json(publications);
+            } else {
+                res.status(409).json({ message: "Publications not found" });
+            }
+        }
+    });
 })
 
+
+function getPendingMoviesFromDB(callback) {
+    connection.query("select * from " + process.env.DB_NAME + ".moviereview where pending = true", function (err, rows) {
+        callback(err, rows);
+    });
+}
 // Implement the pending reviews API endpoint
 app.get('/pending', function (req, res) {
-    // Get a list of pending movie reviews
-    var pending = [
-        { title: 'Superman: Homecoming', release: '2017', score: 10, reviewer: 'Chris Harris', publication: 'International Movie Critic' },
-        { title: 'Wonder Woman', release: '2017', score: 8, reviewer: 'Martin Thomas', publication: 'TheOne' },
-        { title: 'Doctor Strange', release: '2016', score: 7, reviewer: 'Anthony Miller', publication: 'ComicBookHero.com' }
-    ]
-
-    // Send the list of pending movie reviews as a JSON array
-    res.send(pending);
+    getPendingMoviesFromDB(function (err, result) {
+        if (err) {
+            res.status(500).json({ message: err })
+        } else {
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(409).json({ message: "Movies not found" })
+            }
+        }
+    })
 })
 
 // Launch our API Server and have it listen on port 8080.
