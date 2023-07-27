c4953e71150e46247492df403e5a95ef8ae4a728
Jhon Edward Mora <jhonedmora@gmail.com>
2023-07-27 17:16:18 -0500

    Added error display

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ var connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
-});
+}); 
 connection.connect((err) => {
     if (err) {
-        console.log("An error has ocurred connecting to DB, please check credentials");
+        console.log("An error has ocurred connecting to DB, please check credentials" + err);
         throw err;
     }
 })
